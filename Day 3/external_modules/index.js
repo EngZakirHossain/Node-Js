486456439d2af87087950248ea00c36c4ec54fb1
Zakir Trimatric <zakirtrimatric@gmail.com>
2022-10-19 15:50:38 +0600

    npm and custom node module

---
+++ Day 3/external_modules/index.js
@@ -0,0 +1,10 @@
+const getRandomFruitsName = require('random-fruits-name');
+// const moviesNames = require('movies-names');
+// moviesNames.random();
+//
+// console.log(getRandomFruitsName());
+// console.log(getRandomFruitsName('en'));
+
+const randomMovieNames = require('random-movie-names');
+
+console.log(randomMovieNames());
--- Day 3/module_http/index.js
+++ Day 3/module_http/index.js
@@ -2,7 +2,10 @@ const http = require('http');
 const port = 3000;
 const hostName = '127.0.0.1';
 const myServer = http.createServer((req, res)=>{
-    res.end('Hello  i am your first server');
+    // res.end('Hello  i am your first server');
+    res.writeHead(202,{'content-type':'text/html'});
+    res.write("<h1>Server Test</h1>")
+    res.end();
 });
 
 myServer.listen(3000,()=>{
